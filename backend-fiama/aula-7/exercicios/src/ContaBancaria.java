6b0ab72492b55aee94bc204db7ac91fbd0818c9b
alexandretoro <batistorodev@gmail.com>
2026-08-21 21:09:48 -0300

    fix: Correction of the bank account exercise logic

--- backend-fiama/aula-7/exercicios/src/ContaBancaria.java
+++ backend-fiama/aula-7/exercicios/src/ContaBancaria.java
@@ -4,8 +4,7 @@ public class ContaBancaria {
     private double saldo;
     private double limite;
 
-
-    //Construtores
+    //Construtor
     public ContaBancaria(String titular) {
         this.titular = titular;
         saldo = 0;
@@ -14,42 +13,32 @@ public class ContaBancaria {
 
 
     //Criação dos métodos setters
-    public String getTitular(String titular) {return this.titular;}
-    public double getSaldo() {return saldo;}
-    public double getLimite() {return limite;}
+    public String getTitular() { return titular; }
+    public double getSaldo() { return saldo; }
+    public double getLimite() { return limite; }
 
-    public void setTitular(String titular) {this.titular = titular;}
-    public void setSaldo(double saldo) {this.saldo = saldo;}
-    public void setLimite(double limite) {this.limite = limite;}
+    public void setTitular(String titular) { this.titular = titular; }
+    public void setLimite(double limite) { this.limite = limite; }
 
 
     //Métodos da classe
-    public void consultar_saldo (double saldo) {
-        this.saldo = saldo;
-    }
-
     public void depositar(double valor) {
         if (valor > 0) {
             saldo = saldo + valor;
         } else {
             IO.println("O valor para depósito não pode ser negativo!");
         }
-
     }
+
     public void sacar(double valor) {
-        if (saldo < valor) {
-            IO.println("Você está tentando sacar um valor maior do que o saldo da conta!");
-        } else  {
+        if (saldo + limite < valor) {
+            IO.println("Saldo e limite insuficientes!");
+        } else if (saldo < valor) {
+            limite = limite - (valor - saldo);
+            saldo = 0;
+            IO.println("Saldo insuficiente. Limite utilizado!");
+        } else {
             saldo = saldo - valor;
         }
     }
-    public void usar_limite(double valor) {
-        if (saldo < valor) {
-            saldo = saldo - valor;
-        }
-        if (valor < limite) {
-            IO.println("Você está tentando sacar um valor maior do que o limite disponível para sua conta!");
-        }
-    }
-
 }
--- backend-fiama/aula-7/exercicios/src/Main.java
+++ backend-fiama/aula-7/exercicios/src/Main.java
@@ -2,33 +2,55 @@ import static java.lang.IO.*;
 void main()  {
     String titular = readln("Por favor, informe seu nome: ");
     ContaBancaria contaBancaria = new ContaBancaria(titular);
-    IO.println("--- Conta Bancária ---");
-
-    println("Escolha uma opção no menu abaixo: ");
+    println("--- Conta Bancária ---");
+    println("Escolha uma opção abaixo pelo número: ");
     println("0 - Sair");
     println("1 - Abertura de conta");
     println("2 - Consultar saldo de conta");
     println("3 - Realizar Depósito");
     println("4 - Realizar Saque");
 
-    int opcao = Integer.parseInt(readln());
+    int operacao = Integer.parseInt(readln("Informe o número da operação: "));
+//    println("\n");
+    while (operacao != 0) {
 
-    while (opcao != 0) {
-        switch (opcao) {
-            case 1:
-                println("Você está abrindo uma nova conta: ");
-                println(contaBancaria.getTitular(titular) + " o saldo da sua conta é: " + contaBancaria.getSaldo() + " e o limite da conta é: " + contaBancaria.getLimite());
-            case 2:
-                println(contaBancaria.getSaldo());
-            case 3:
-                println("Informe um valor para depositar: ");
-                contaBancaria.depositar(Double.parseDouble(readln()));
-                println("Seu saldo atual é: " + contaBancaria.getSaldo());
-            case 4:
-                println("Informe um valor para sacar: ");
-                contaBancaria.sacar(Double.parseDouble(readln()));
-                println("Seu saldo atual é: " + contaBancaria.getSaldo());
+        if (operacao == 1) {
+            println("Você está abrindo uma nova conta: ");
+            println(contaBancaria.getTitular() + " o saldo da sua conta é: " + contaBancaria.getSaldo() + " e o limite da conta é: " + contaBancaria.getLimite() + "\n");
         }
+        else if (operacao == 2) {
+            println("O saldo em conta é: " + contaBancaria.getSaldo() + " e o limite da conta é: " + contaBancaria.getLimite() + "\n");
+            }
+        else if(operacao == 3) {
+            print("Informe um valor para depositar: ");
+            contaBancaria.depositar(Double.parseDouble(readln()));
+            println("Seu saldo atual é: " + contaBancaria.getSaldo() + "\n");
+            }
+        else if (operacao == 4) {
+            print("Informe um valor para sacar: ");
+            double valor = Double.parseDouble(readln());
+
+            if (valor <= contaBancaria.getSaldo() + contaBancaria.getLimite()) {
+                contaBancaria.sacar(valor);
+                println("Seu saldo atual é: " + contaBancaria.getSaldo() + " e seu limite agora é: " + contaBancaria.getLimite() + "\n");
+            } else {
+                println("Saldo da conta e limite insuficientes!\n");
+            }
+        }
+        else {
+                println("Opção inválida!\n");
+            }
+
+        // Mostra o menu novamente para a próxima operação
+        println("Escolha uma opção abaixo pelo número: ");
+        println("0 - Sair");
+        println("1 - Abertura de conta");
+        println("2 - Consultar saldo de conta");
+        println("3 - Realizar Depósito");
+        println("4 - Realizar Saque");
+        operacao = Integer.parseInt(readln("Escolha uma operação: "));
     }
+
+    println("Você saiu da sua conta!");
 
 }
